93f70d190c0f0022675eb44f30194b0141a8cf75
drzeno <drzeno@yeah.net>
2015-09-13 21:40:18 +0800

    need to finish

---
+++ mergingSort.c
@@ -0,0 +1,7 @@
+/*********************************
+    > File Name: mergingSort.c
+    > Author: Zeno
+    > Mail:340562424@qq.com 
+    > Created Time: 2015年09月13日 星期日 21时39分46秒
+ ********************************/
+//yes, it's empty.
